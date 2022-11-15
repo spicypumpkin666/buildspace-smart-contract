edc0757a25c8f8173f35408581405ec3dc1ca453
spicypumpkin666 <spicypumpkin666@proton.me>
2022-11-15 16:28:42 +0100

    borked the contract

--- my-wave-portal/contracts/WavePortal.sol
+++ my-wave-portal/contracts/WavePortal.sol
@@ -6,6 +6,7 @@ import "hardhat/console.sol";
 
 contract WavePortal {
 	uint256 totalWaves;
+	string[] waveAddresses;
 
 	constructor() {
 		console.log("Yo yo, I am a contract and I am smurt.");
@@ -19,6 +20,14 @@ contract WavePortal {
 	function getTotalWaves() public view returns (uint256) {
 		console.log("We have %d total waves!", totalWaves);
 		return totalWaves;
+	}
+
+	function storeAddresses(address) public {
+		waveAddresses.push(address);
+	}
+
+	function getAddresses() public view returns (string[]) {
+		return waveAddresses;
 	}
 }
 
--- my-wave-portal/scripts/run.js
+++ my-wave-portal/scripts/run.js
@@ -1,22 +1,42 @@
 /*
+// get wallet address of contract owner 
 // compile contract, generate files to work w/ contract in artifacts/
 // Hardhat creates local eth network for this contract 
 // wait till contract deployed
 // logs address of the deployed contract
+// logs address of deployer
+// calls functions in contract: get total waves, call wave function, get total waves again
 //NB hre = hardhat runtime environment
 */
 
 const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal"); 
     const waveContract = await waveContractFactory.deploy(); 
     await waveContract.deployed(); 
-    console.log("Contract deployed to:", waveContract.address); 
+
+    console.log("Contract deployed to:", waveContract.address);
+    console.log("Contract deployed by:", owner.address);
+    
+    const firstWaveTxn = await waveContract.wave();
+    await firstWaveTxn.wait();
+
+    await waveContract.storeAddresses(firstWaveTxn.address);
+
+    const secondWaveTxn = await waveContract.connect(randomPerson).wave();
+    await secondWaveTxn.wait();
+
+    await waveContract.storeAddresses(secondWaveTxn.address);
+
+    await waveContract.getTotalWaves();
+
+    console.log("waves from:", waveContract.getAddresses())
 };
 
 const runMain = async () => {
     try {
         await main();
-        ProcessingInstruction.exit(0); // exit Node process without error
+        process.exit(0); // exit Node process without error
     } catch (error) {
         console.log(error);
         process.exit(1); // exist Node process while indicating 'Uncaught Fatal Exception' error 
